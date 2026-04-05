ab495d4ddd0ab53f468046f6ca8d49cd30c397f4
Darshaan Aghicha <dsaghicha211@gmail.com>
2026-04-05 12:45:22 +0530

    feat: OTP verification in VerifyOtpView

--- server/core/api/auth/urls.py
+++ server/core/api/auth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, VerifyOtpView
 
 urlpatterns = [
     path(
         "signup/",
         SignUpView.as_view(),
         name="signup",
-    )
+    ),
+    path("verify-otp/", VerifyOtpView.as_view(), name="verify-otp"),
 ]
--- server/core/api/auth/views.py
+++ server/core/api/auth/views.py
@@ -1,10 +1,16 @@
-from django.contrib.messages import success
+from datetime import timedelta
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
+from django.utils.timezone import now
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.api.auth.serializers import SignUpSerializer
 from core.models import Otp
+
+User = get_user_model()
 
 
 class SignUpView(APIView):
@@ -30,4 +36,68 @@ class SignUpView(APIView):
         return Response(
             {"messages": serializer.errors, "success": False},
             status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class VerifyOtpView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request) -> Response:
+        user_id = request.data.get("user_id")
+        otp = request.data.get("otp")
+        intent = request.data.get("intent")
+
+        if not all([user_id, otp, intent]):
+            return Response(
+                {"message": "Missing Required Fields", "success": False},
+                status=status.HTTP_206_PARTIAL_CONTENT,
+            )
+
+        user = User.objects.filter(id=user_id).first()
+        if not user:
+            return Response(
+                {"message": "User Not Found", "success": False},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if user.is_active:
+            return Response(
+                {"message": "User Account Activated", "success": True},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        last_otp = Otp.objects.filter(user_id=user_id, intent=intent).last()
+        if not last_otp:
+            return Response(
+                {"message": "No active OTP found", "success": False},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if last_otp.issued_at < (now() - timedelta(minutes=5)):
+            last_otp.delete()
+            return Response(
+                {
+                    "message": "Entered OTP is expired. Kindly use resend OTP to generate new one!",
+                    "success": False,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        otp_matches = check_password(otp, last_otp.otp_hash)
+
+        if not otp_matches:
+            return Response(
+                {"message": "Invalid OTP", "success": False},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if intent == "registration":
+            user.is_active = True
+            user.verified_at = now()
+            user.updated_at = now()
+            user.save()
+
+        last_otp.delete()
+        return Response(
+            {"message": "OTP verified", "success": True}, status=status.HTTP_200_OK
         )
